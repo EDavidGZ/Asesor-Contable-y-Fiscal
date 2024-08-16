5d8c58db419de8a80270ccb9fcdc957b0214b6e0
EDavidGZ <jgar05202415@gmail.com>
2024-08-16 09:13:19 -0600

    t212

--- src/Services.jsx
+++ src/Services.jsx
@@ -22,11 +22,11 @@ function Services() {
     <section id="services" className="py-20 bg-gray-100">
       <div className="container mx-auto text-center">
         <h3 className="text-4xl font-bold mb-12 text-blue-900">Servicios</h3>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-8">
 {
           services.map((service) => (
 
-          <div key={service.id} className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow px-6">
+          <div key={service.id} className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow ">
             <FaCheckCircle className="text-blue-600 text-4xl mb-4 mx-auto" />
             <h4 className="text-2xl font-semibold mb-4">{service.title}</h4>
             <p className="text-gray-700">{service.about}</p>
